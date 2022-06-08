60d1fd6e41c191b20417660939123cd8a901f3de
Danil Galkin <mehanikdanil@gmail.com>
2022-06-08 13:18:18 +0700

    изменение на data class

--- app/src/main/java/meh/daniel/com/movieunderbeer/domain/entities/recyclerfeed/FeedGenreGroup.kt
+++ app/src/main/java/meh/daniel/com/movieunderbeer/domain/entities/recyclerfeed/FeedGenreGroup.kt
@@ -2,4 +2,4 @@ package meh.daniel.com.movieunderbeer.domain.entities.recyclerfeed
 
 import meh.daniel.com.movieunderbeer.presentation.adapter.common.Item
 
-class FeedGenreGroup( val listGenre: List<FeedGenre>) : Item
+data class FeedGenreGroup( val listGenre: List<FeedGenre>) : Item
